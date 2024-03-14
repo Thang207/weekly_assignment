4388952b95c371e30ad704605d08e7261084899c
ThangPC <22521334@ms.uit.edu.vn>
2024-03-14 10:39:56 +0700

    add: file explorer assignment

---
+++ file_explorer/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace file_explorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.back_btn = new System.Windows.Forms.Button();
+            this.forward_btn = new System.Windows.Forms.Button();
+            this.browser_btn = new System.Windows.Forms.Button();
+            this.path_text = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // back_btn
+            // 
+            this.back_btn.Location = new System.Drawing.Point(12, 14);
+            this.back_btn.Name = "back_btn";
+            this.back_btn.Size = new System.Drawing.Size(75, 23);
+            this.back_btn.TabIndex = 0;
+            this.back_btn.Text = "Go back";
+            this.back_btn.UseVisualStyleBackColor = true;
+            this.back_btn.Click += new System.EventHandler(this.back_btn_Click);
+            // 
+            // forward_btn
+            // 
+            this.forward_btn.Location = new System.Drawing.Point(93, 14);
+            this.forward_btn.Name = "forward_btn";
+            this.forward_btn.Size = new System.Drawing.Size(75, 23);
+            this.forward_btn.TabIndex = 1;
+            this.forward_btn.Text = "Go Forward";
+            this.forward_btn.UseVisualStyleBackColor = true;
+            this.forward_btn.Click += new System.EventHandler(this.forward_btn_Click);
+            // 
+            // browser_btn
+            // 
+            this.browser_btn.Location = new System.Drawing.Point(680, 12);
+            this.browser_btn.Name = "browser_btn";
+            this.browser_btn.Size = new System.Drawing.Size(75, 23);
+            this.browser_btn.TabIndex = 2;
+            this.browser_btn.Text = "Browser";
+            this.browser_btn.UseVisualStyleBackColor = true;
+            this.browser_btn.Click += new System.EventHandler(this.browser_btn_Click);
+            // 
+            // path_text
+            // 
+            this.path_text.Location = new System.Drawing.Point(228, 14);
+            this.path_text.Multiline = true;
+            this.path_text.Name = "path_text";
+            this.path_text.Size = new System.Drawing.Size(446, 20);
+            this.path_text.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(190, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Path:";
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Location = new System.Drawing.Point(12, 43);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(741, 590);
+            this.webBrowser.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(765, 645);
+            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.path_text);
+            this.Controls.Add(this.browser_btn);
+            this.Controls.Add(this.forward_btn);
+            this.Controls.Add(this.back_btn);
+            this.Name = "Form1";
+            this.Text = "File explorer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button back_btn;
+        private System.Windows.Forms.Button forward_btn;
+        private System.Windows.Forms.Button browser_btn;
+        private System.Windows.Forms.TextBox path_text;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.WebBrowser webBrowser;
+    }
+}
+
